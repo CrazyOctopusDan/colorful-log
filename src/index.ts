235f154f1681a531b66a93cb0117240a2a5248d2
ZhangYi <840271360@qq.com>
2023-04-10 14:41:08 +0800

    feat(0.0.1): 增加了基础功能；

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+/*
+ * @Description: 这是默认设置
+ */
+import ColorfulLogClass, { colorfulLog, materialColorfulLog } from './Colorful-log';
+import * as styleValue from './style';
+
+export {
+  styleValue,
+  colorfulLog, 
+  materialColorfulLog
+};
+
+export default ColorfulLogClass;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,38 @@
+/*
+ * @Author: ZhangYi 840271360@qq.com
+ * @Date: 2023-04-10 13:59:31
+ * @LastEditors: ZhangYi 840271360@qq.com
+ * @LastEditTime: 2023-04-10 14:15:06
+ * @FilePath: /colorful-log/vite.config.ts
+ */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: 'modules',
+    outDir: 'dist',
+    minify: false,
+    rollupOptions: {
+      input: ['src/index.ts'],
+      output: [{
+        format: 'es',
+        entryFileNames: '[name].js',
+        preserveModules: true,
+        dir: 'es',
+        preserveModulesRoot: 'src'
+      }, {
+        format: 'cjs',
+        entryFileNames: '[name].js',
+        preserveModules: true,
+        dir: 'lib',
+        preserveModulesRoot: 'src'
+      }]
+    },
+    lib: {
+      entry: './index.ts',
+      formats: ['es', 'cjs']
+    },
+  },
   plugins: [react()],
 })
