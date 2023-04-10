896368498e5511616e25d6550e9f695e366be39e
ZhangYi <840271360@qq.com>
2023-04-10 17:52:26 +0800

    修改了package.json

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,20 @@
-declare interface IColorfulLogConfig {
-    logBy?: string;
-    logName?: string;
-    preBlockColor?: string;
-    nextBlockColor?: string;
-    padding?: number;
-    borderRadius?: number;
-    fontColor?: string;
-}
+declare module '@tms/co-colorful-log' {
+    
 
-declare class ColorfulLogClass {
-    constructor(config: IColorfulLogConfig)
-}
+    export interface IColorfulLogConfig {
+        logBy?: string;
+        logName?: string;
+        preBlockColor?: string;
+        nextBlockColor?: string;
+        padding?: number;
+        borderRadius?: number;
+        fontColor?: string;
+    }
+
+    export function colorfulLog(logBy: string, logName: string, ...logData: any[]): typeof console;
+    
+    export function materialColorfulLog({logName, type, isLinearGradient}: {logName: string, type: string, isLinearGradient: boolean}, ...data: any[]): typeof console
+    export default class ColorfulLogClass {
+        constructor(config: IColorfulLogConfig)
+    }
+  }
